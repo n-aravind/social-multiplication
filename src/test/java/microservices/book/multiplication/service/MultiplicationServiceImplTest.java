44f72db0f6b58b1fe895ab1ac5366f54dcc98fdc
Aravind <aravindnareddy@gmail.com>
2019-02-10 20:37:50 -0700

    Implemented RandomServiceGeneratorImpl

---
+++ src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -0,0 +1,36 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+
+public class MultiplicationServiceImplTest {
+
+    private MultiplicationServiceImpl multiplicationServiceImpl;
+
+    @Mock
+    private RandomGeneratorService randomGeneratorService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService);
+    }
+
+    @Test
+    public void createRandomMultiplicationTest() {
+        given(randomGeneratorService.generateRandomFactor()).willReturn(50,30);
+
+        Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
+
+        assertThat(multiplication.getFactorA()).isEqualTo(50);
+        assertThat(multiplication.getFactorB()).isEqualTo(30);
+        assertThat(multiplication.getResult()).isEqualTo(1500);
+    }
+}
